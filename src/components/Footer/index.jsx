680c1372f96f777fe4924ba5747777aa1e9c522b
rishusingh022 <rishusingh022@gmail.com>
2023-03-02 11:27:59 +0530

    feat(feature4) : logic for theme change

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import './Footer.css';
 function Footer() {
+  const [theme, setTheme] = React.useState('black');
+  React.useEffect(() => {
+    document.body.style.setProperty('--theme-color', `${theme}`);
+  });
+  const handletheme = (inputTheme) => {
+    setTheme(inputTheme);
+  };
   return (
     <div className="footer">
       <p>
         <strong>THEMES</strong>
       </p>
       <div className="themes">
-        <div className="button"></div>
-        <div className="button"></div>
-        <div className="button"></div>
+        <div
+          onClick={() => {
+            handletheme('purple');
+          }}
+          className="purple-button"
+        ></div>
+        <div
+          onClick={() => {
+            handletheme('blue');
+          }}
+          className="blue-button"
+        ></div>
+        <div
+          onClick={() => {
+            handletheme('grey');
+          }}
+          className="grey-button"
+        ></div>
       </div>
     </div>
   );
